e15314d77793931dfff45a8029e926f36c8e5509
fnaoto <f.naoto832@gmail.com>
2020-03-27 00:32:47 +0900

    Updates

---
+++ precache-manifest.ee588ba37cd238ae8d7bf3324e493846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb903ba6bb2ce7d3c7b3c1f8c8516e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cfb60653878e431352e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d7e9fdd5e3f93db59299",
+    "url": "/static/js/2.877cf490.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.877cf490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfb60653878e431352e",
+    "url": "/static/js/main.7aa49fd2.chunk.js"
+  },
+  {
+    "revision": "b77ba9d3ed7149ca75b3",
+    "url": "/static/js/runtime-main.b350602b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46816facf12207f5cfd58d8e5ee8a817.js"
+  "/precache-manifest.ee588ba37cd238ae8d7bf3324e493846.js"
 );
 
 self.addEventListener('message', (event) => {
