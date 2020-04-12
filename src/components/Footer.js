70fce033080ed42dc68250705c7cd4562f3e4d06
fnaoto <f.naoto832+github@gmail.com>
2020-04-12 19:36:12 +0900

    Fix layout to bright image.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import '../App.css';
+import GitHub from "../png/github.png";
+import Qiita from "../png/qiita.png";
+import Wantedly from "../png/wantedly.png";
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer></footer>
+      <footer className="App-footer">
+        <h3 className="App-underline">Links</h3>
+        <h3>
+          <a href="https://qiita.com/fnaoto">
+            <img src={Qiita} className="App-logo" alt="@Qiita" />
+          </a>
+          <a href="https://www.wantedly.com/users/47203885">
+            <img src={Wantedly} className="App-logo" alt="@Wantedly" />
+          </a>
+          <a href="https://github.com/fnaoto">
+            <img src={GitHub} className="App-logo" alt="@GitHub" />
+          </a>
+        </h3>
+      </footer>
     );
   }
 }
